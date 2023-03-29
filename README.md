5099cd10607e134413d1f0e2bc29ac9325a08abc
Andrew McMullin <mcmullinboy15@gmail.com>
2023-03-29 01:49:13 -0600

    Used Mom's example to perfect this to the correct calculations. See README.md in this commit to see how the >, <, >=, and <= made a difference and how rounding made a difference

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # GoalHome
+
+```py
+    ((minutes > day_start) & (minutes < day_end))
+```
+>  <     Don't want to exclude both
+>  <=    Off in Day_OT, but is the one i want
+>= <     Best!!
+>= <=    Don't want to include both
+
+Night_OT Pay: `Only off due to not rounding at Dollar calculation (g["Night Rate"] * 1.5) * g["Night_OT"]`
+
+Last Name   First Name  |  Day      Day Pay |  Night    Night Pay |  Day_OT    Day_OT Pay |  Night_OT   Night_OT Pay | Paddington Bonus |  Pay      Pay_OT | Total Hours  Total Pay |
+SEALII      FANNY       |  57.9     1273.8  |  22.1     353.6     | [8.05]    [265.65]    | [2.12]     [50.8_]       | [0.0]            |  1627.4   316.45 | 90.17        1943.85   |
+SEALII      FANNY       | [57.95]  [1274.9] | [22.05]  [352.8]    |  8.08      266.75     |  2.08       50.0         | [0.0]            | [1627.7]  316.75 | 90.17        1944.45   |
+SEALII      FANNY       | [57.95]  [1274.9] | [22.05]  [352.8]    | [8.05]    [265.65]    | [2.12]     [50.8_]       | [0.0]            | [1627.7]  316.45 | 90.17        1944.15   |
+SEALII      FANNY       |  58.0     1276.0  |  22.0     352.0     |  8.08      266.75     |  2.08       50.0         | [0.0]            |  1628.0   316.75 | 90.17        1944.75   |
+
+
+# Night_OT rounded here: `(g["Night Rate"] * 1.5) * g["Night_OT"].round(2)`
+SEALII      FANNY   1274.9      265.65      352.8         50.88  1627.7  316.53  57.95    8.05  22.05     2.117               0.0       90.167    1944.23
+# Rounded here: `Within calcWeek return pd.Series(...).round(2)`
+## THIS IS PERFECT!!!!
+SEALII      FANNY   1274.9      265.65      352.8         50.88  1627.7  316.53  57.95    8.05  22.05      2.12               0.0        90.17    1944.23
--- payroll.py
+++ payroll.py
@@ -4,6 +4,8 @@ import typing
 import pathlib
 import argparse
 import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 from datetime import datetime, timedelta, date, time
 
 
@@ -26,13 +28,17 @@ def parse_args():
     parser.add_argument("--pay-rate-sheet-name", type=str, default="Pay Rate")
 
     parser.add_argument("--verify-pay-rates",  action="store_true")
-    parser.add_argument("--verify-day-night",  action="store_true")
     parser.add_argument("--verify-weighted",   action="store_true")
     parser.add_argument("--verify-paddington", action="store_true")
 
     parser.add_argument("-v", "--verbose", action="store_true")
     return parser.parse_args()
 
+def p(x, prefix="", func=lambda x: x):
+    print(prefix)
+    print(func(x))
+    # input("...")
+    return x
 
 def main(args):
 
@@ -40,7 +46,7 @@ def main(args):
         print(args)
 
 
-    sheet = pd.read_excel(cd / args.timesheet, sheet_name=args.timesheet_sheet_name, parse_dates=["Date", "Start Time", "End Time"])
+    sheet: pd.DataFrame = pd.read_excel(cd / args.timesheet, sheet_name=args.timesheet_sheet_name, parse_dates=["Date", "Start Time", "End Time"], usecols=["First Name", "Last Name", "Date", "Start Time", "End Time", "Regular", "Schedule"])
     pay_rates = pd.read_excel(cd / args.pay_rate_file, sheet_name=args.pay_rate_sheet_name)
     if args.verify_pay_rates:
         print(pay_rates.to_string())
@@ -51,62 +57,112 @@ def main(args):
     sheet["Last Name"] = sheet["Last Name"].str.upper()
     sheet["First Name"] = sheet["First Name"].str.upper()
 
-    # Rename column "Regular" to "Total"
-    sheet["Total"] = sheet["Regular"]
-
 
     # Sort Sheet by Date
     sheet.sort_values(by=["Date"], inplace=True)
-    
-
-    # Find Nights: Count the number of minutes that are between 10PM - 6AM and divide by 60
-    def countNightHours(x):
-        r = pd.date_range(x["Start Time"], x["End Time"], freq="min")
-        night_minutes = r[(time(22,00) <= r.time) | (r.time < time(6,00))]
-        return len(night_minutes) / 60
-
-    sheet["Night"] = sheet.apply(countNightHours, axis=1)
-    
-
-    # Subract Nights from Total and put in "Day"
-    sheet["Day"] = sheet["Total"] - sheet["Night"]
 
 
-    # "Week" = {first week: 1, second week: 2} (7 days)
-    sheet["Week"] = sheet["Start Time"].dt.strftime("%U").astype(int)
-    sheet["Week"] = sheet["Week"] - min(sheet["Week"]) + 1
+    def person_to_minutes(person):
+        start_times = pd.to_datetime(person["Start Time"]).dt.floor('min')
+        end_times = pd.to_datetime(person["End Time"])
+
+        def func(start, end, schedule):
+            rng = pd.date_range(start, end, freq=freq)[:-1]
+            # p((len(rng), len(rng) / toHours), prefix="Push In:")
+            return pd.DataFrame({'date': rng, 'Schedule': [schedule] * len(rng)})
+        
+        return pd.concat([func(start, end, schedule) for start, end, schedule in zip(start_times, end_times, person["Schedule"])]) # .reset_index(drop=True)
+
+    day_start, day_end = time(6,00), time(22,00)
+    def minutes_to_daynight_hours(minutes) -> typing.Tuple[int, int]:
+        p(pd.concat([minutes, (minutes > day_start), (minutes < day_end), (minutes > day_start) & (minutes < day_end)], axis=1), prefix="MINUTES: ")
+        p((len(minutes), (minutes > day_start).sum(), (minutes < day_end).sum(), ((minutes > day_start) & (minutes < day_end)).sum()), prefix="MINUTES: ")
+        day_minutes = ((minutes >= day_start) & (minutes < day_end)).sum()
+        p((len(minutes), len(minutes) / 60), prefix="minutes: ")
+        p((len(minutes[(minutes > day_start)]), len(minutes[(minutes > day_start)]) / 60), prefix="day_minutes - (minutes > day_start): ")
+        p((len(minutes[(minutes < day_end)]), len(minutes[(minutes < day_end)]) / 60), prefix="day_minutes - (minutes < day_end): ")
+        p((day_minutes, day_minutes / 60), prefix="day_minutes: ")
+        p((day_minutes / toHours, (len(minutes) - day_minutes), (len(minutes) - day_minutes) / toHours ), prefix="day_minutes: ")
+        return day_minutes / toHours, (len(minutes) - day_minutes) / toHours    
 
 
-    # Print Day and Night Infromation
-    if args.verify_day_night:
-        print(sheet[["Date", "Last Name", "First Name", "Day", "Night", "Total"]].to_string())                             # All info
-        print()
-        print(sheet.groupby(["Last Name", "First Name", "Week"])[["Day", "Night", "Total"]].sum().to_string())     # Grouped by person and week
-        print()
-        print(sheet.groupby(["Last Name", "First Name"])[["Day", "Night", "Total"]].sum().to_string())             # Grouped by only person
-        return
-    
+    def calcPerson(person):
+        p(person, prefix="Person:")
+        minutes = person_to_minutes(person)
+        p(round(len(minutes) / toHours, 2), prefix="Hours:")
 
-    # Get their(a person's) Day hours, and night Hours, GroupedBy week (sum of those hours)
-    g = sheet.groupby(["Last Name", "First Name", "Week"])[["Total", "Day", "Night"]].sum()
+        minutes['Week'] = minutes['date'].dt.strftime("%U").astype(int)
 
+        def calcWeek(week):
+
+            paddington_hours = len(week[week["Schedule"] == "Paddington"])
+            p((paddington_hours, paddington_hours / 60), prefix="Paddington: ")
+
+            p((len(week['date']) > forty, str(len(week['date'])) + ' > ' + str(forty)), prefix="isOverTime: ")
+            isOverTime = len(week['date']) > forty
+            if not isOverTime:
+                (day, night) = minutes_to_daynight_hours(week['date'].dt.time)
+                return pd.Series({"Day": day, "Night": night, "Day_OT": 0, "Night_OT": 0, "Paddington Hours": paddington_hours / toHours}).round(2)
+
+            isOT = week['date'] >= week['date'].iloc[forty]
+            p((len(week[~isOT]), len(week[~isOT]) / 60), prefix="isNotOT: ")
+            p((len(week[isOT]), len(week[isOT]) / 60), prefix="isOT: ")
+            (day, night)       = minutes_to_daynight_hours(week[~isOT]['date'].dt.time)
+            (day_ot, night_ot) = minutes_to_daynight_hours(week[isOT]['date'].dt.time)
+            p((day_ot, night_ot), prefix="OT: ")
+            return pd.Series({"Day": day, "Night": night, "Day_OT": day_ot, "Night_OT": night_ot, "Paddington Hours": paddington_hours / toHours}).round(2)
+            
+        return minutes.groupby(['Week'], group_keys=True).apply(calcWeek)
+
+
+    # freq, toHours, forty = "S", (60 * 60), (40 * 60 * 60)
+
+    # st = datetime.now()
+    # print("Start Time: ", st)
+    # g1 = sheet.groupby(["Last Name", "First Name"]).apply(calcPerson)
+    # print("Duration: ", datetime.now() - st)
+
+
+    freq, toHours, forty = "min", (60), (40 * 60)
+
+    st = datetime.now()
+    print("Start Time: ", st)
+    g2 = sheet.groupby(["Last Name", "First Name"]).filter(lambda x: x["First Name"].iloc[0] == "FANNY").groupby(["Last Name", "First Name"]).apply(calcPerson)
+    # g2 = sheet.groupby(["Last Name", "First Name"]).apply(calcPerson)
+    print("Duration: ", datetime.now() - st)
+
+    # print(g1)
+    print(g2)
+    # print("Day: \t", round((g1["Day"] - g2["Day"]).sum(skipna=True), 2), "\t$"+str(round((g1["Day"] - g2["Day"]).sum(skipna=True) * 25, 2)))
+    # print("Night: \t", round((g1["Night"] - g2["Night"]).sum(skipna=True), 2), "\t$"+str(round((g1["Night"] - g2["Night"]).sum(skipna=True) * 25, 2)))
+    # print("Day_OT: \t", round((g1["Day_OT"] - g2["Day_OT"]).sum(skipna=True), 2), "\t$"+str(round((g1["Day_OT"] - g2["Day_OT"]).sum(skipna=True) * 25, 2)))
+    # print("Night_OT: \t", round((g1["Night_OT"] - g2["Night_OT"]).sum(skipna=True), 2), "\t$"+str(round((g1["Night_OT"] - g2["Night_OT"]).sum(skipna=True) * 25, 2)))
+
+    # gp = g1
+    g = g2.groupby(["Last Name", "First Name", "Week"]).sum()
+
+    # g["Hours"] = g["Day"] + g["Night"]
+    # g["Hours_OT"] = g["Day_OT"] + g["Night_OT"]
+    g["Total Hours"] = g["Day"] + g["Day_OT"] + g["Night"] + g["Night_OT"]
 
     # Weighted Overtime
     g[["Day Rate", "Night Rate"]] = pay_rates.groupby(["LAST", "FIRST"]).max()[["Day Rate", "Night Rate"]]
 
-    g["Pay"] = (g["Day Rate"] * g["Day"]) + (g["Night Rate"] * g["Night"])
-
-    g["OT Hours"] = (g["Total"] - 40).clip(lower=0)
-    g["Weighted Rate"] = g["Pay"] / g["Total"]
-    g["Weighted OT"] = g["OT Hours"] * (g["Weighted Rate"] * 0.5)
+    g["Day Pay"]      = g["Day Rate"]  * g["Day"]
+    g["Day_OT Pay"]   = (g["Day Rate"] * 1.5) * g["Day_OT"]
+    g["Night Pay"]    = g["Night Rate"] * g["Night"]
+    g["Night_OT Pay"] = (g["Night Rate"] * 1.5) * g["Night_OT"]
+    p((g[["Day Rate", "Night Rate"]], g["Night_OT"]), prefix="Rates: ")
+    input("...")
+    g["Pay"] = g["Day Pay"] + g["Night Pay"]
+    g["Pay_OT"] = g["Day_OT Pay"] + g["Night_OT Pay"]
 
     if args.verify_weighted:
-        print(g[["Day Rate", "Day", "Night Rate", "Night", "Pay", "OT Hours", "Weighted Rate", "Weighted OT"]].to_string())
+        print(g[["Day Pay", "Day_OT Pay", "Night Pay", "Night_OT Pay", "Pay", "Pay_OT", "Total Hours"]].to_string())
         return
 
-
-    # Number of hours worked per schedule from "Total" (times 2, paddington only)
-    g[["Paddington Bonus"]] = sheet[sheet["Schedule"] == "Paddington"].groupby(["Last Name", "First Name", "Week"])[["Total"]].sum() * 2
+    # Number of hours worked per schedule from "Total Hours" (times 2, paddington only)
+    g[["Paddington Bonus"]] = g[["Paddington Hours"]] * 2
     if args.verify_paddington:
         print(g[["Paddington Bonus"]].to_string())
         return
@@ -114,26 +170,74 @@ def main(args):
 
     """ OUTPUT: Total Day, Total Night, Total Weighted OT, Paddington Bonus, TOTAL PAY (PAY1+PAY2+WOT1+WOT2+P1+P2) """
     d = g.groupby(["Last Name", "First Name"]).sum().round(3)
-    d["Total Pay"] = d[["Pay", "Weighted OT", "Paddington Bonus"]].sum(axis=1)
+    d["Total Pay"] = d[["Pay", "Pay_OT", "Paddington Bonus"]].sum(axis=1)
 
 
-    output = d[["Day", "Night", "Weighted OT", "Paddington Bonus", "Total Pay"]]
+    output = d[["Day Pay", "Day_OT Pay", "Night Pay", "Night_OT Pay", "Pay", "Pay_OT", "Day", "Day_OT", "Night", "Night_OT", "Paddington Bonus", "Total Hours", "Total Pay"]].reset_index()
     print(output.to_string())
     print()
 
     # Save File
     output_filename = args.timesheet.replace(".xlsx", f"{args.output_tag}.xlsx")
     writer = pd.ExcelWriter(cd / output_filename)
-    output.to_excel(writer, sheet_name=args.output_sheet_name, na_rep='NaN')
+    output.to_excel(writer, sheet_name=args.output_sheet_name, na_rep='NaN', startrow=1, header=False, index=False)
     
     # Set Column width
     for col_idx in range(len(output.index.names) + len(output.columns)):
-        writer.sheets[args.output_sheet_name].set_column(col_idx, col_idx, 20)
+        column_name = output.columns[col_idx - len(output.index.names)]
+        column_width = max(output[column_name].astype(str).map(len).max(), len(column_name))
+        writer.sheets[args.output_sheet_name].set_column(col_idx, col_idx, column_width+4)
+
+
+    workbook = writer.book
+    worksheet = writer.sheets[args.output_sheet_name]
+
+
+
+    # Get the dimensions of the dataframe.
+    (max_row, max_col) = output.shape
+
+    # Create a list of column headers, to use in add_table().
+    column_settings = [{'header': column} for column in output.columns]
+
+    # Add the Excel table structure. Pandas will add the data. (+2 for my index)
+    worksheet.add_table(0, 0, max_row, (max_col - 1), {'columns': column_settings, 'autofilter': False, 'banded_columns': False, 'style': 'Table Style Medium 9'})
+
+    # https://xlsxwriter.readthedocs.io/format.html#format
+
+
+
+
+    # light_gray_format = workbook.add_format({'bg_color': '#F4F4F4', 'border_color': '#646464', 'valign': 'vcenter', 'border': 1})
+    # white_format = workbook.add_format({'bg_color': '#FFFFFF', 'border_color': '#646464', 'valign': 'vcenter', 'border': 1})
+
+    # header_format = workbook.add_format({'bold': True,
+    #                                     'text_wrap': True, 
+    #                                     'align': 'center',
+    #                                     'valign': 'vcenter',
+    #                                     'bg_color': '#008080',
+    #                                     'font_color': '#ffffff',
+    #                                     'border': 1,
+    #                                     'border_color': '#646464'})
+
+    # worksheet.conditional_format('A1:C5', {'type': 'formula',
+    #                                         'criteria': '=ROW()=1',
+    #                                         'format': header_format})
+
+    # worksheet.conditional_format('A2:C5', {'type': 'formula',
+    #                             'criteria': '=EVEN(ROW())=ROW()',
+    #                             'format': white_format})
+
+    # worksheet.conditional_format('A2:C5', {'type': 'formula',
+    #                             'criteria': '=ODD(ROW())=ROW()',
+    #                             'format': light_gray_format})
+
 
     writer.close()
 
 
 if __name__ == "__main__":
+
     try:
         args = parse_args()
 
@@ -153,8 +257,9 @@ if __name__ == "__main__":
         print()
 
     except Exception as e:
-        print("ERROR : "+str(e))
+        print("ERROR : " + str(e))
         print()
+        raise ValueError(str(e))
 
     finally:
         input("Press Enter to exit ...")
